f69eeb04a860e09881f39443a3c1e472f30f2e64
Luke Bermingham <Luke Bermingham>
2016-12-11 02:21:53 +1000

    Added screen shake

---
+++ Assets/Scripts/Damagable.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damagable : MonoBehaviour {
+
+    private Vector3 cameraRestorePosition;
+
+	// Use this for initialization
+	void Start () {
+        cameraRestorePosition = Camera.main.transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D other){
+        Damager damager = other.GetComponent<Damager>();
+        if(damager != null)
+        {
+            Camera.main.GetComponent<ScreenShakeEffect>().StartScreenShake();
+        }
+    }
+
+}
---
+++ Assets/Scripts/ScreenShakeEffect.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (Camera))]
+public class ScreenShakeEffect : MonoBehaviour {
+
+    [SerializeField]
+    private float ShakeDurationSeconds = 0.3f;
+
+    [SerializeField]
+    private float MaxShakeIntensity = 1;
+
+    private Camera screenShakeCamera;
+    private Vector3 cameraRestorePosition;
+    private float shakeTimeElapsed = float.MaxValue;
+
+	// Use this for initialization
+	void Start () {
+        this.screenShakeCamera = GetComponent<Camera>();
+        this.cameraRestorePosition = screenShakeCamera.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(shakeTimeElapsed < ShakeDurationSeconds)
+        {
+            ShakeScreen();
+            shakeTimeElapsed += Time.deltaTime;
+            //once time is elapsed, restore the camera position
+            if(shakeTimeElapsed >= ShakeDurationSeconds)
+            {
+                screenShakeCamera.transform.position = cameraRestorePosition;
+            }
+        }
+	}
+
+    private void ShakeScreen()
+    {
+        float shakeMagnitude = (1 - (shakeTimeElapsed / ShakeDurationSeconds)) * MaxShakeIntensity;
+        Vector3 movedCamPos = this.cameraRestorePosition + (Random.onUnitSphere * shakeMagnitude);
+        screenShakeCamera.transform.position = movedCamPos;
+    }
+
+    /**
+     * Public methods, so other scripts can shake the screen
+     **/
+     public void StartScreenShake()
+    {
+        shakeTimeElapsed = 0f;
+    }
+
+}
