df3ecf731f8febe927c985f8663791f1aec9143c
Andrew <therealandrewjoy@gmail.com>
2016-12-13 00:14:47 +1000

    Add Fatass enemy

---
+++ Assets/Scripts/AnimationSpeed.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public sealed class AnimationSpeed : MonoBehaviour
+{
+    [SerializeField]
+    private Animator animator = null;
+
+    [SerializeField]
+    private float speed = 1.0f;
+
+    private void Start()
+    {
+        this.animator.speed = this.speed;
+    }
+}
